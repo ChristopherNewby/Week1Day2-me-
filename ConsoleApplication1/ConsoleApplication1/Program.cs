c98732f99c35fc0f51a3b7a6ec3760a1ed210408
Christopher Newby <Christopher Newby@DESKTOP-3F7RENE>
2016-09-27 14:48:41 -0400

    Week1Day2Homework

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            string y;
+            int newNum;
+
+             Console.WriteLine("Can you enter a number between 1 and 100?");
+             y = Console.ReadLine();
+            //x = Convert.ToInt32(Console.ReadLine());
+            if (Int32.TryParse(y, out newNum))
+            {
+                for (int i = 1; i < newNum + 1; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine("fizzbuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+
+                        Console.WriteLine("fizz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine("buzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            Console.ReadLine();
+
+
+
+
+            
+        }
+
+    }
+}
